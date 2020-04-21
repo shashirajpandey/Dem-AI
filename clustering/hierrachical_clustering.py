e9a5592a996678aa9ec3d38c2afae10ffc904f95
minhnguyen <nhatminhhcmut@gmail.com>
2020-04-21 18:42:16 +0900

    Clean and update hierrachical_clustering.py

--- clustering/hierrachical_clustering.py
+++ clustering/hierrachical_clustering.py
@@ -5,6 +5,7 @@ from scipy.cluster.hierarchy import dendrogram
 from sklearn.datasets import load_iris
 from sklearn.cluster import AgglomerativeClustering
 from flearn.utils.BTree import Node
+from clustering.Setting import *
 
 ''' sklearn.cluster.AgglomerativeClustering
 linkage{“ward”, “complete”, “average”, “single”}, default=”ward”
@@ -51,130 +52,185 @@ def cal_linkage_matrix(model):
     return n_samples, linkage_matrix
 
 
-def retrieve_leaves(cluster_head, n_samples=150):
+def retrieve_leaves(model, cluster_head):
     leaves = []
-    if (cluster_head < n_samples):
+    if (cluster_head < N_clients):
         leaves.append(cluster_head)
     else:
-        for idx in model.children_[cluster_head - n_samples]:
-            leaves = leaves + retrieve_leaves(idx)
+        for idx in model.children_[cluster_head - N_clients]:
+            leaves = leaves + retrieve_leaves(model, idx)
     return leaves
 
+def retrieve_cluster_head(model, cluster_head):
+    if (cluster_head >= N_clients):
+        return model.children_[cluster_head - N_clients]
 
-def retrieve_cluster_head(cluster_head, n_samples=150):
-    if (cluster_head >= n_samples):
-        return model.children_[cluster_head - n_samples]
-
-
-def create_nodes(Parent):
+def create_nodes(model, Parent, Clients):
     if (Parent.level == 1):
-        childs = retrieve_leaves(Parent["_id"])
-        tmp_c = []
-        for c in childs:
+        childs_idx = retrieve_leaves(model, Parent["_id"])
+        childs = []
+        for c in childs_idx:
             child = Clients[c]
             child.parent = Parent
-            tmp_c.append(child)
-        Parent.childs = tmp_c
+            childs.append(child)
+        Parent.childs = childs
     else:
-        cluster_heads = model.children_[Parent["_id"] - n_clients]
+        cluster_heads = model.children_[Parent["_id"] - N_clients]
         Child1 = Node(_id=cluster_heads[0], parent=Parent, level=Parent.level - 1)
         Child2 = Node(_id=cluster_heads[1], parent=Parent, level=Parent.level - 1)
-        if (cluster_heads[0] <= n_clients):
+        if (cluster_heads[0] <= N_clients):
             Child1 = Clients[cluster_heads[0]]
-            Child1.parent = Root
+            Child1.parent = Parent
         else:
-            create_nodes(Child1)
-        if (cluster_heads[1] <= n_clients):
+            create_nodes(model,Child1,Clients)
+        if (cluster_heads[1] <= N_clients):
             Child2 = Clients[cluster_heads[1]]
-            Child2.parent = Root
+            Child2.parent = Parent
         else:
-            create_nodes(Child2)
+            create_nodes(model,Child2,Clients)
 
         Childs = [Child1, Child2]
         Parent.childs = Childs
 
+def iris_clustering():
+    iris = load_iris()
+    X = iris.data
+    print("Iris Data Shape:",iris.data.shape)
 
-iris = load_iris()
-X = iris.data
+    # setting distance_threshold=0 ensures we compute the full tree.
+    result = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
 
-# setting distance_threshold=0 ensures we compute the full tree.
-result = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
+    ### OR Plot Only get labels
+    # model1 = result.fit_predict(X)
+    # print(model1)
 
-### OR Plot Only get labels
-# model1 = result.fit_predict(X)
-# print(model1)
+    # OR Plot
+    model = result.fit(X)
+    print("After Clustering:",model.labels_)
+    return model
 
-# OR Plot
-model = result.fit(X)
-print(model.labels_)
-# print(model.children_)
-# print(model.distances_)
+def weight_clustering(X):
+    # setting distance_threshold=0 ensures we compute the full tree.
+    #linkage {“ward”, “complete”, “average”, “single”}, default =”ward”
+    #connectivity {default: "euclidean”, “l1”, “l2”, “manhattan”, “cosine”, or “precomputed”}
+    result = AgglomerativeClustering(distance_threshold=0, n_clusters=None, connectivity=None, linkage='ward')
 
-numb_samples, rs_linkage_matrix = cal_linkage_matrix(model)
+    ### OR Plot Only get labels
+    # model1 = result.fit_predict(X)
+    # print(model1)
 
-K_Levels = 3
-# for level in range(1, K_Levels):
-#     print("=> GENERALIZED LEVEL", K_Levels - level, "-------")
-#     rs_dendrogram = dendrogram(rs_linkage_matrix, truncate_mode='level', p=level)
-#     cluster_heads = rs_dendrogram['leaves']
-#
-#     tmp_dev = []
-#     for g_idx in cluster_heads:
-#         if (g_idx < numb_samples):
-#             print("Leaf:", g_idx)
-#             # Node(g_idx,)
-#         else:
-#             if (level == K_Levels - 1):
-#                 print("All leaves:", retrieve_leaves(g_idx, numb_samples))
-#                 c_id = retrieve_leaves(g_idx, numb_samples)
-#                 # for c in c_id:
-#                 #     tmp_dev.append()
-#             else:
-#                 h_id = retrieve_cluster_head(g_idx, numb_samples)
-#                 # print(Node() )
-#                 print("Children:", retrieve_cluster_head(g_idx, numb_samples))
+    # OR Plot
+    model = result.fit(X)
+    print("After Clustering:",model.labels_)
+    return model
 
-n_clients = 150
-rs_dendrogram = dendrogram(rs_linkage_matrix, truncate_mode='level', p=1)
+def gradient_clustering(X):
+    # setting distance_threshold=0 ensures we compute the full tree.
+    #linkage {“ward”, “complete”, “average”, “single”}, default =”ward”
+    #connectivity {default: "euclidean”, “l1”, “l2”, “manhattan”, “cosine”, or “precomputed”}
+    result = AgglomerativeClustering(distance_threshold=0, n_clusters=None, connectivity="cosine", linkage='ward')
 
-cluster_heads = model.children_[-1]
-print(cluster_heads)
-Clients = []
-for c in range(n_clients):
-    Clients.append(Node(_id=c,_type="Client", level=0))
+    ### OR Plot Only get labels
+    # model1 = result.fit_predict(X)
+    # print(model1)
+
+    # OR Plot
+    model = result.fit(X)
+    print("After Clustering:",model.labels_)
+    return model
 
 
-Root = Node(_id="Root", parent=None, level=K_Levels+1)
-Child1 = Node(_id=cluster_heads[0], parent=Root, level=K_Levels)
-Child2 = Node(_id=cluster_heads[1], parent=Root, level=K_Levels)
-if (cluster_heads[0] <= n_clients):
-    Child1 = Clients[cluster_heads[0]]
-    Child1.parent = Root
-else:
-    create_nodes(Child1)
-if (cluster_heads[1] <= n_clients):
-    Child2 = Clients[cluster_heads[1]]
-    Child2.parent = Root
-else:
-    create_nodes(Child2)
 
-Childs = [Child1, Child2]
-Root.childs = Childs
-# print(Root.childs[0].childs[0].childs[0].childs)
-print(Root.count_clients())
-# print(Root.childs[0].childs)
 
-# for n in Clients:
-#     print(n)
+def iris_test(model):
+    # print(model.children_)
+    # print(model.distances_)
+    numb_samples, rs_linkage_matrix = cal_linkage_matrix(model)
+    k_Levels = 2
+    for level in range(1, 2):
+        print("=> GENERALIZED LEVEL", k_Levels - level, "-------")
+        rs_dendrogram = dendrogram(rs_linkage_matrix, truncate_mode='level', p=level)
+        cluster_heads = rs_dendrogram['leaves']
 
-# # Plot the corresponding dendrogram
-# plt.clf()
-#
-# # change p value to 5 if we want to get 5 levels
-# plt.title('Hierarchical Clustering Dendrogram')
-# rs_dendrogram = dendrogram(rs_linkage_matrix, truncate_mode='level', p=4)
-#
-# print(rs_dendrogram['ivl'])  # x_axis of dendrogram => index of nodes or (Number of points in clusters (i))
-# print(rs_dendrogram['leaves'])  # merge points
-# plt.xlabel("index of node or (Number of leaves in each cluster).")
-# plt.show()
+        for g_idx in cluster_heads:
+            if (g_idx < numb_samples):
+                print("Leaf:", g_idx)
+                # Node(g_idx,)
+            else:
+                if (level == k_Levels - 1):
+                    all_leaves = retrieve_leaves(model, g_idx, numb_samples)
+                    print("All leaves:", all_leaves, " => Len:",len(all_leaves))
+                    # c_id = retrieve_leaves(g_idx, numb_samples)
+                else:
+                    # h_id = retrieve_cluster_head(g_idx, numb_samples)
+                    print("Children:", retrieve_cluster_head(model, g_idx, numb_samples))
+
+
+def plot_dendrogram(rs_linkage_matrix):
+    # Plot the corresponding dendrogram
+    plt.clf()
+
+    # change p value to 5 if we want to get 5 levels
+    plt.title('Hierarchical Clustering Dendrogram')
+    rs_dendrogram = dendrogram(rs_linkage_matrix, truncate_mode='level', p=4)
+
+    print(rs_dendrogram['ivl'])  # x_axis of dendrogram => index of nodes or (Number of points in clusters (i))
+    print(rs_dendrogram['leaves'])  # merge points
+    plt.xlabel("index of node or (Number of leaves in each cluster).")
+    plt.show()
+
+
+def tree_construction(model, Clients):
+    # rs_dendrogram = dendrogram(rs_linkage_matrix, truncate_mode='level', p=1)
+    cluster_heads = model.children_[-1]
+    print(cluster_heads)
+
+    Root = Node(_id="Root", parent=None, level=K_Levels+1)
+    Child1 = Node(_id=cluster_heads[0], parent=Root, level=K_Levels)
+    Child2 = Node(_id=cluster_heads[1], parent=Root, level=K_Levels)
+    if (cluster_heads[0] <= N_clients):
+        Child1 = Clients[cluster_heads[0]]
+        Child1.parent = Root
+    else:
+        create_nodes(model, Child1, Clients)
+    if (cluster_heads[1] <= N_clients):
+        Child2 = Clients[cluster_heads[1]]
+        Child2.parent = Root
+    else:
+        create_nodes(model, Child2, Clients)
+
+    Childs = [Child1, Child2]
+    Root.childs = Childs
+
+    return Root
+
+
+if __name__ == "__main__":
+    K_Levels = 3
+    N_clients = 20
+    Weight_dimension = 10
+
+    Clients = []
+    for c in range(N_clients):
+        Clients.append(Node(_id=c, _type="Client", level=0))
+
+
+
+    weights_matrix = np.random.rand(N_clients, Weight_dimension)
+    model = weight_clustering(weights_matrix)
+    # gradient_matrix = np.random.rand(N_clients, Weight_dimension)
+    # model = gradient_clustering(gradient_matrix)
+
+    Tree_Root = tree_construction(model, Clients)
+    # print(Tree_Root.childs[0].childs[0].childs[0].childs)
+    # print(Tree_Root.childs[0].childs)
+    # for n in Clients:
+    #     print(n)
+
+    print("Number of agents in tree:", Tree_Root.count_clients())
+    print("Number of agents in level K:",Tree_Root.childs[0].count_clients(), Tree_Root.childs[1].count_clients())
+    print("Number of agents Group 1 in level K-1:",Tree_Root.childs[0].childs[0].count_clients(), Tree_Root.childs[0].childs[1].count_clients())
+
+    # model = iris_clustering()
+    # N_clients = 150
+    # iris_test(model)
