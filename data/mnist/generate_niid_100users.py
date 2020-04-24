3926269647fcec4363bc47b1b29714c7bd2869ba
minhnguyen <nhatminhhcmut@gmail.com>
2020-04-24 19:34:04 +0900

    Update generated data for 50, 100 users

--- data/mnist/generate_niid_100users.py
+++ data/mnist/generate_niid_100users.py
@@ -7,7 +7,7 @@ import os
 
 random.seed(1)
 np.random.seed(1)
-NUM_USERS = 10
+NUM_USERS = 100
 NUM_LABELS = 2
 # Setup directory for train/test data
 train_path = './data/train/mnist_train.json'
@@ -31,7 +31,7 @@ def sort_by_target(mnist):
 # mnist = fetch_openml('MNIST original', data_home='./data')
 mnist = fetch_openml('mnist_784', version=1, cache=True)
 mnist.target = mnist.target.astype(np.int8)  # fetch_openml() returns targets as strings
-sort_by_target(mnist)  # fetch_openml() returns an unsorted dataset
+
 mu = np.mean(mnist.data.astype(np.float32), 0)
 sigma = np.std(mnist.data.astype(np.float32), 0)
 mnist.data = (mnist.data.astype(np.float32) - mu)/(sigma+0.001)
@@ -43,47 +43,57 @@ for i in trange(10):
 print("\nNumb samples of each label:\n", [len(v) for v in mnist_data])
 
 ###### CREATE USER DATA SPLIT #######
-# Assign 100 samples to each user
+# Assign 150 samples to each user
 X = [[] for _ in range(NUM_USERS)]
 y = [[] for _ in range(NUM_USERS)]
 idx = np.zeros(10, dtype=np.int64)
 for user in range(NUM_USERS):
     for j in range(NUM_LABELS):  # 3 labels for each users
-        # l = (2*user+j)%10
-        l = (user + j) % 10 
-        print("L:",l)
+        #l = (2*user+j)%10
+        l = (user + j) % 10    # round robin style to assign labels keep user_index in 10 base or 100 base
+        print("L:", l)
         X[user] += mnist_data[l][idx[l]:idx[l]+10].tolist()
         y[user] += (l*np.ones(10)).tolist()
-        idx[l] += 10
+        idx[l] += 5
 
-print("IDX1:", idx) # counting samples for each labels
+print("IDX1:", idx)  # counting samples for each labels
 
 # Assign remaining sample by power law
 user = 0
 props = np.random.lognormal(
-    0, 2., (10, NUM_USERS, NUM_LABELS))  # last 5 is 5 labels
+    0, 1., (10, NUM_USERS, NUM_LABELS))  # last 5 is 5 labels
+props = np.array([[[len(v)-1000]] for v in mnist_data]) * \
+    props/np.sum(props, (1, 2), keepdims=True)
 # print("here:",props/np.sum(props,(1,2), keepdims=True))
-props = np.array([[[len(v)-100]] for v in mnist_data])*props/np.sum(props,(1,2), keepdims=True)
+#props = np.array([[[len(v)-100]] for v in mnist_data]) * \
+#    props/np.sum(props, (1, 2), keepdims=True)
 #idx = 1000*np.ones(10, dtype=np.int64)
 # print("here2:",props)
-
-# for user in trange(NUM_USERS):
-for user in range(NUM_USERS):
-    for j in range(NUM_LABELS):  # 4 labels for each users
+for user in trange(NUM_USERS):
+    for j in range(NUM_LABELS):  # the same 3 labels for each users but new remaining data
         # l = (2*user+j)%10
-        l = ( user + j) % 10
-
-        #num_samples = int(props[l,user//int(NUM_USERS/10),j]) *10
-        num_samples = int(props[l, user, j]) * NUM_USERS
-        #num_samples = min(num_samples,200)
+        l = (user + j) % 10     # round robin style to assign labels keep user_index in 10 base or 100 base
+        num_samples = int(props[l, user//int(NUM_USERS/10), j])
+        num_samples = int(num_samples )  # Scale up number of samples
         # print(num_samples)
+        # numran1 = random.randint(10, 50)     # num_samples plus 50, 100, 200
+        # numran2 = random.randint(1, 2)        # scale up num_samples to 2, 5, 10 times
+        # # num_samples = (num_samples) * numran2 + numran1  #Scale up number of samples by factors of numran2, numran1
+        # num_samples = num_samples  + numran1  # Scale up number of samples by factors of numran2, numran1
+        if(NUM_USERS <= 10):
+            num_samples = num_samples * 2
+        # num_samples = int(props[l,user//int(NUM_USERS/10),j])
+        # num_samples = min(num_samples,200)
+
         if idx[l] + num_samples < len(mnist_data[l]):
             X[user] += mnist_data[l][idx[l]:idx[l]+num_samples].tolist()
             y[user] += (l*np.ones(num_samples)).tolist()
             idx[l] += num_samples
+            print("check len os user:", user, j,
+                  "len data", len(X[user]), num_samples)
 
 print("IDX2:", idx) # counting samples for each labels
-
+print("Remaining samples added:", sum(idx))
 # Create data structure
 train_data = {'users': [], 'user_data':{}, 'num_samples':[]}
 test_data = {'users': [], 'user_data':{}, 'num_samples':[]}
@@ -97,7 +107,7 @@ for i in range(NUM_USERS):
     random.shuffle(combined)
     X[i][:], y[i][:] = zip(*combined)
     num_samples = len(X[i])
-    train_len = int(0.75*num_samples)
+    train_len = int(0.8*num_samples)  #Test 80%
     test_len = num_samples - train_len
     
     train_data['users'].append(uname) 
@@ -107,8 +117,10 @@ for i in range(NUM_USERS):
     test_data['user_data'][uname] = {'x': X[i][train_len:], 'y': y[i][train_len:]}
     test_data['num_samples'].append(test_len)
 
-print("Num_samples:", train_data['num_samples'])
-print("Total_samples:",sum(train_data['num_samples']))
+print("Numb_training_samples:", train_data['num_samples'])
+print("Total_training_samples:",sum(train_data['num_samples']))
+print("Numb_testing_samples:", test_data['num_samples'])
+print("Total_testing_samples:",sum(test_data['num_samples']))
     
 with open(train_path,'w') as outfile:
     json.dump(train_data, outfile)
--- data/mnist/generate_niid_50users.py
+++ data/mnist/generate_niid_50users.py
@@ -8,7 +8,7 @@ import os
 random.seed(1)
 np.random.seed(1)
 NUM_USERS = 50
-NUM_LABELS = 3
+NUM_LABELS = 2
 # Setup directory for train/test data
 train_path = './data/train/mnist_train.json'
 test_path = './data/test/mnist_test.json'
@@ -54,14 +54,14 @@ for user in range(NUM_USERS):
         print("L:", l)
         X[user] += mnist_data[l][idx[l]:idx[l]+10].tolist()
         y[user] += (l*np.ones(10)).tolist()
-        idx[l] += 10
+        idx[l] += 5
 
 print("IDX1:", idx)  # counting samples for each labels
 
 # Assign remaining sample by power law
 user = 0
 props = np.random.lognormal(
-    0, 2., (10, NUM_USERS, NUM_LABELS))  # last 5 is 5 labels
+    0, 1., (10, NUM_USERS, NUM_LABELS))  # last 5 is 5 labels
 props = np.array([[[len(v)-1000]] for v in mnist_data]) * \
     props/np.sum(props, (1, 2), keepdims=True)
 # print("here:",props/np.sum(props,(1,2), keepdims=True))
@@ -74,11 +74,12 @@ for user in trange(NUM_USERS):
         # l = (2*user+j)%10
         l = (user + j) % 10     # round robin style to assign labels keep user_index in 10 base or 100 base
         num_samples = int(props[l, user//int(NUM_USERS/10), j])
-
-        numran1 = random.randint(10, 50)     # num_samples plus 50, 100, 200
-        numran2 = random.randint(1, 2)        # scale up num_samples to 2, 5, 10 times
-        # num_samples = (num_samples) * numran2 + numran1  #Scale up number of samples by factors of numran2, numran1
-        num_samples = num_samples  + numran1  # Scale up number of samples by factors of numran2, numran1
+        num_samples = int(num_samples * 0.45)  # Scale down number of samples
+        # print(num_samples)
+        # numran1 = random.randint(10, 50)     # num_samples plus 50, 100, 200
+        # numran2 = random.randint(1, 2)        # scale up num_samples to 2, 5, 10 times
+        # # num_samples = (num_samples) * numran2 + numran1  #Scale up number of samples by factors of numran2, numran1
+        # num_samples = num_samples  + numran1  # Scale up number of samples by factors of numran2, numran1
         if(NUM_USERS <= 10):
             num_samples = num_samples * 2
         # num_samples = int(props[l,user//int(NUM_USERS/10),j])
