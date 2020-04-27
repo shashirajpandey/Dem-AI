8e25d64425b748756aef147cb835dd4886d23c69
trindqb <manhcuong018@gmail.com>
2020-04-27 19:14:54 +0900

    add ---> save_febavg to file

--- clustering/Setting.py
+++ clustering/Setting.py
@@ -1,5 +1,5 @@
 "FIX PROGRAM SETTINGS"
-PLOT_PATH = "./figs/"
+PLOT_PATH = "../figs/"
 K_Levels = 3  #2, 3
 N_clients = 50
 NUM_GLOBAL_ITERS = 10 #100
@@ -8,8 +8,8 @@ CLUSTER_METHOD = "weight"
 # CLUSTER_METHOD = "gradient" #"gradient"  #"weight" or "gradient"
 MODEL_TYPE = "cnn" #"cnn" or "mclr"
 # RUNNING_ALG= "fedavg"
-RUNNING_ALG= "fedprox"
-# RUNNING_ALG = "demavg"
+# RUNNING_ALG= "fedprox"
+RUNNING_ALG = "demavg"
 # RUNNING_ALG = "demprox"
 
 
